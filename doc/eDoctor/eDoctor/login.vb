5dcd87bcdbcf7dd771ca9aa268b0b67284a3531d
Unknown <julio.romotishca@gmail.com>
2017-03-07 03:33:09 -0600

    Inicio de pantallas

---
+++ doc/eDoctor/eDoctor/login.vb
@@ -0,0 +1,10 @@
+﻿Public Class login
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+    End Sub
+
+    Private Sub Label2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Label2.Click
+
+    End Sub
+End Class
